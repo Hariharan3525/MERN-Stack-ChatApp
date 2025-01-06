e1982455ea5ab4435307471f3b44204c960114ef
Hariharan <123272238+Hariharan3525@users.noreply.github.com>
2025-01-06 18:06:29 +0530

    Updated the project

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,10 +23,10 @@ app.use(cookieParser())
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 
-app.use(express.static(path.join(__dirname,'../../frontend/build')))
+app.use(express.static(path.join(__dirname,'../../frontend/dist')))
 
 app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname,'../../frontend/build','index.html'))
+    res.sendFile(path.join(__dirname,'../../frontend/dist','index.html'))
 })
 
 server.listen(port,()=>{
